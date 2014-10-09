21541ef8224ff9028234c728fd3e92a5c23629d6
James Macfie <james@macfie.co.nz>
2014-10-10 08:15:58 +1300

    Setting the watch folder correctly for SASS

--- themes/bento/gruntfile.js
+++ themes/bento/gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
 		},
 		watch: {
 			styles: {
-				files: 'sass/**/*.scss',
+				files: 'stylesheets/**/*.scss',
 				tasks: ['sass'],
 				options: {
 					debounceDelay: 250
